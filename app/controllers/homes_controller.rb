bd9caf42d65c4d0924f4bf7c58561eb2f3e7543e
Yasunori Sakada <jpeggogei@gmail.com>
2019-11-15 01:04:02 +0900

    slim to erb

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,12 @@
+class HomesController < ApplicationController
+  def top
+    @new_tasks = Task.order(created_at: :desc).limit(12)
+    @update_tasks = Task.order(updated_at: :desc).limit(12)
+  end
+
+  def about
+  end
+
+  def mypage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tasks
-  get 'top' => "home#top"
+  get 'top' => "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "about" => "home#about"
-  get "mypage" => "home#mypage"
-  root to: "home#top"
+  get "about" => "homes#about"
+  get "mypage" => "homes#mypage"
+  root to: "homes#top"
 end
